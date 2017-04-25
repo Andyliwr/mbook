55f298811fca7db034f7e716286cd714c51f601e
andyliwr <andyliwr@outlook.com>
2017-04-25 17:07:45 +0800

    配置后台管理的路由

--- api/server/boot/root.js
+++ api/server/boot/root.js
@@ -1,6 +1,8 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  // router.get('/', server.loopback.status());
+  console.log(__dirname);
+  router.use(server.loopback.static(__dirname.replace('/server/boot', '') + '/client'))
   server.use(router);
 };
