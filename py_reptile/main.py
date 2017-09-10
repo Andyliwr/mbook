ac40410383a7ff3f4f4b1fd8f68c8df5df3cd447
andyliwr <andyliwr@outlook.com>
2017-09-10 23:31:30 +0800

    使用pandas和jieba包分析豆瓣影评

--- py_reptile/main.py
+++ py_reptile/main.py
@@ -1,9 +1,13 @@
 
-#!/usr/bin/env python
-#-*-coding:utf-8*-
+# !/usr/bin/env python
+# -*-coding:utf-8*-
 from urllib import request
 from bs4 import BeautifulSoup as bs
 import re
+# 分词包
+import jieba
+import pandas as pd
+
 resp = request.urlopen('https://movie.douban.com/nowplaying/hangzhou/')
 html_data = resp.read().decode('utf-8')
 soup = bs(html_data, 'html.parser')
@@ -12,7 +16,7 @@ nowplaying_movie_list = nowplaying_movie[0].find_all('li', class_='list-item')
 # print(nowplaying_movie_list[0])
 
 # 获取电影列表
-nowplaying_list = [] 
+nowplaying_list = []
 for item in nowplaying_movie_list:
     nowplaying_dict = {}
     nowplaying_dict['id'] = item['data-subject']
@@ -21,7 +25,7 @@ for item in nowplaying_movie_list:
         nowplaying_list.append(nowplaying_dict)
 # print(nowplaying_list)
 
-#获取影评
+# 获取影评
 requrl = 'https://movie.douban.com/subject/'+nowplaying_list[0]['id']+'/comments?start=0&limit=20'
 resp = request.urlopen(requrl)
 comment_data = resp.read().decode('utf-8')
@@ -44,4 +48,10 @@ pattern = re.compile(r'[\u4e00-\u9fa5]+')
 filter_data = re.findall(pattern, comments)
 cleaned_comments = ''.join(filter_data)
 
-print(cleaned_comments)
+# print(cleaned_comments)
+segment = jieba.lcut(cleaned_comments)
+words_df = pd.DataFrame({'segment': segment})
+# quoting=3全不引用
+stopwords = pd.read_csv("E:\\project\\graduationDesign\\py_reptile\\stopwords.txt", index_col=False, quoting=3, sep="\t", names=['stopword'], encoding='utf-8')
+words_df = words_df[~words_df.segment.isin(stopwords.stopword)]
+print(words_df.head())
